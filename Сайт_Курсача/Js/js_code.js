f3c3bc26cf13cc78f5a98b99260ff88ebd2fd7dd
Mihail <65620172+mmo0307@users.noreply.github.com>
2020-09-28 17:05:44 +0300

    body_flex

---
+++ Сайт_Курсача/Js/js_code.js
@@ -0,0 +1,11 @@
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("bn").style.top = "0";
+    } else {
+        document.getElementById("bn").style.top = "-140px";
+    }
+}
+
